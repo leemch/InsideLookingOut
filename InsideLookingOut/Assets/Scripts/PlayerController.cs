1fe6d5e5b3899a67af8fe6c0d93ffba94ed33f22
leemch <lee7mchale@gmail.com>
2018-09-18 20:44:25 -0700

    Added placeholder sprites and basic movement

---
+++ InsideLookingOut/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    public float moveSpeed;
+    private Rigidbody2D myRigidbody;
+
+    public float jumpSpeed;
+
+	// Use this for initialization
+	void Start () {
+        myRigidbody = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxisRaw("Horizontal") > 0f)
+        {
+            myRigidbody.velocity = new Vector3(moveSpeed, myRigidbody.velocity.y, 0f);
+
+        }else if (Input.GetAxisRaw("Horizontal") < 0f)
+        {
+            myRigidbody.velocity = new Vector3(-moveSpeed, myRigidbody.velocity.y, 0f);
+        }
+        else
+        {
+            myRigidbody.velocity = new Vector3(0f, myRigidbody.velocity.y, 0f);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            myRigidbody.velocity = new Vector3(myRigidbody.velocity.x, jumpSpeed, 0f);
+        }
+    }
+}
