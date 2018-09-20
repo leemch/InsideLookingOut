8edb5ec572b0c7326f52600d7d1aa80103142598
leemch <lee7mchale@gmail.com>
2018-09-19 20:24:46 -0700

    new Updates

--- InsideLookingOut/Assets/Scripts/PlayerController.cs
+++ InsideLookingOut/Assets/Scripts/PlayerController.cs
@@ -10,30 +10,52 @@ public class PlayerController : MonoBehaviour {
 
     public float jumpSpeed;
 
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+
+    public bool isGrounded;
+
+    private Animator myAnim;
+
+
+
 	// Use this for initialization
 	void Start () {
         myRigidbody = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
         if (Input.GetAxisRaw("Horizontal") > 0f)
         {
             myRigidbody.velocity = new Vector3(moveSpeed, myRigidbody.velocity.y, 0f);
+            transform.localScale = new Vector3(1f, 1f, 1f);
 
         }else if (Input.GetAxisRaw("Horizontal") < 0f)
         {
             myRigidbody.velocity = new Vector3(-moveSpeed, myRigidbody.velocity.y, 0f);
+            transform.localScale = new Vector3(-1f, 1f, 1f);
         }
         else
         {
             myRigidbody.velocity = new Vector3(0f, myRigidbody.velocity.y, 0f);
         }
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             myRigidbody.velocity = new Vector3(myRigidbody.velocity.x, jumpSpeed, 0f);
         }
+
+        myAnim.SetFloat("Speed", Mathf.Abs(myRigidbody.velocity.x));
+        myAnim.SetBool("Grounded", isGrounded);
+
+
+
     }
 }
