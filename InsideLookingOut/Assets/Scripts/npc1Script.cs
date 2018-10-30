dc883d80b9ec0a4c08db6c5542e7e0eff6f63c1f
leemch <lee7mchale@gmail.com>
2018-10-30 13:04:21 -0700

    Revert "Merge branch 'master' of https://github.com/leemch/InsideLookingOut"
    This reverts commit 81592d18bf18154ae57ece259f850b8306c7448c, reversing
    changes made to b34b6068a484d58e43c529efbdd3da974dfa1940.

--- InsideLookingOut/Assets/Scripts/npc1Script.cs
+++ InsideLookingOut/Assets/Scripts/npc1Script.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class npc1Script : MonoBehaviour {
 
+<<<<<<< HEAD
     public dialogue dialogue;
 
     public void TriggerDialog()
     {
         FindObjectOfType<dialogueManager>().StartDialogue(dialogue);
     }
+=======
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+>>>>>>> parent of 81592d1... Merge branch 'master' of https://github.com/leemch/InsideLookingOut
 }
