be7248e18b2fdca6b493803b797fa44e72a285cb
leemch <lee7mchale@gmail.com>
2018-10-29 22:04:47 -0700

    update

---
+++ InsideLookingOut/Assets/Scripts/dialogueManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialogueManager : MonoBehaviour {
+
+    private Queue<string> sentences;
+
+	// Use this for initialization
+	void Start () {
+        sentences = new Queue<string>();
+	}
+	
+
+    public void StartDialogue(dialogue dialogue)
+    {
+        Debug.Log("Starting convo with" + dialogue.name);
+
+        sentences.Clear();
+
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        Debug.Log(sentence);
+    }
+
+    void EndDialogue()
+    {
+        Debug.Log("End of Convo");
+    }
+}
--- InsideLookingOut/Assets/Scripts/npc1Script.cs
+++ InsideLookingOut/Assets/Scripts/npc1Script.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class npc1Script : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public dialogue dialogue;
+
+    public void TriggerDialog()
+    {
+        FindObjectOfType<dialogueManager>().StartDialogue(dialogue);
+    }
 }
