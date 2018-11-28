a4026720c75c8700040a0e2eb104c46979278190
leemch <lee7mchale@gmail.com>
2018-11-27 18:59:27 -0800

    update

---
+++ InsideLookingOut/Assets/Scripts/fallingConeScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallingConeScript : MonoBehaviour {
+
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic ;
+            //Destroy(gameObject);
+        }
+    }
+
+}
