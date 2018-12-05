6319836270a9d68aea65cae5d7b2a8391c7a2ec4
leemch <lee7mchale@gmail.com>
2018-12-04 21:54:00 -0800

    update

--- InsideLookingOut/Assets/Scripts/boss3Controller.cs
+++ InsideLookingOut/Assets/Scripts/boss3Controller.cs
@@ -36,6 +36,9 @@ public class boss3Controller : MonoBehaviour {
     public bool isCasting;
 
     public GameObject water;
+    public GameObject beam;
+
+    public int attackStage = 1;
 
 
     // Use this for initialization
@@ -49,7 +52,7 @@ public class boss3Controller : MonoBehaviour {
         isCasting = false;
         //player = lvlManager.player;
 
-        state = 1;
+        state = 0;
     }
 
     // Update is called once per frame
@@ -77,34 +80,41 @@ public class boss3Controller : MonoBehaviour {
                 case 0:
                 //idle
                 anim.Play("sharkIdle");
+                if (!isCasting)
+                {
+                    StartCoroutine("idleWait");
+                }
+                    
+                
                 break;
 
             case 1:
                 //charging
 
-                anim.Play("sharkRun");
-
-
-                
-                // Compute the next position -- straight flight
-                Vector3 nextPos = Vector3.MoveTowards(transform.position, currentTargetPoint.transform.position, moveSpeed * Time.deltaTime);
-
-                transform.position = nextPos;
-
-                //Arrived at default point
-                if (nextPos == defaultPoint.transform.position)
+                if (!isCasting)
                 {
-                    moveToEnd();
-                    transform.localScale = new Vector3(.5f, .5f, .5f);
-                    state = 0 ;
-                    state = 2;
-                }
+                    anim.Play("sharkRun");
 
-                // reach the spikes; damage boss
-                if (nextPos == endPoint.transform.position)
-                {
-                    moveToDefault();
-                    
+                    // Compute the next position -- straight flight
+                    Vector3 nextPos = Vector3.MoveTowards(transform.position, currentTargetPoint.transform.position, moveSpeed * Time.deltaTime);
+
+                    transform.position = nextPos;
+
+                    //Arrived at default point
+                    if (nextPos == defaultPoint.transform.position)
+                    {
+                        moveToEnd();
+                        transform.localScale = new Vector3(.5f, .5f, .5f);
+                        attackStage++;
+                        state = 0;
+
+                    }
+
+                    // reach the spikes; damage boss
+                    if (nextPos == endPoint.transform.position)
+                    {
+                        StartCoroutine("hurtBoss");
+                    }
                 }
                 break;
 
@@ -117,7 +127,10 @@ public class boss3Controller : MonoBehaviour {
                 break;
 
             case 3:
-
+                if (!isCasting)
+                {
+                    StartCoroutine("castBeamAttack");
+                }
                 break;
 
             case 4:
@@ -129,6 +142,24 @@ public class boss3Controller : MonoBehaviour {
 
         }
 
+    }
+
+
+    public IEnumerator castWaterAttack()
+    {
+        isCasting = true;
+        anim.Play("sharkWave");
+
+        water.GetComponent<Animator>().Play("waterAnim");
+
+        yield return new WaitForSeconds(12f);
+
+
+        state = 0;
+        isCasting = false;
+        attackStage++;
+
+        yield return null;
     }
 
 
@@ -169,17 +200,15 @@ public class boss3Controller : MonoBehaviour {
 
 
 
-    public IEnumerator castWaterAttack()
+    public IEnumerator hurtBoss()
     {
         isCasting = true;
-        anim.Play("sharkWave");
+        sprRender.color = Color.red;
 
-                water.GetComponent<Animator>().Play("waterAnim");
+        yield return new WaitForSeconds(3f);
 
-        yield return new WaitForSeconds(12f);
-
-
-        state = 0;
+        sprRender.color = Color.white;
+        moveToDefault();
         isCasting = false;
 
         yield return null;
@@ -192,12 +221,36 @@ public class boss3Controller : MonoBehaviour {
         anim.Play("sharkWave");
 
     
-        //water.GetComponent<Animator>().Play("waterAnim");
+        beam.GetComponent<Animator>().Play("beamAnim");
 
-        yield return new WaitForSeconds(12f);
+        yield return new WaitForSeconds(8f);
 
 
         state = 0;
+        isCasting = false;
+        attackStage++;
+
+        yield return null;
+    }
+
+
+    public IEnumerator idleWait()
+    {
+        isCasting = true;
+
+
+
+        yield return new WaitForSeconds(3f);
+        
+        if (attackStage > 3)
+        {
+            attackStage = 1;
+        }  
+
+            
+
+
+        state = attackStage;
         isCasting = false;
 
         yield return null;
