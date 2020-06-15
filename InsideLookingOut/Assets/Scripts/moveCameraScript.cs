13011f4ecbd11ee8bf735d5df228d68af50cf0a3
leemch <lee7mchale@gmail.com>
2020-06-15 10:18:14 -0700

    fixed 3d cone spotlight model not displaying

---
+++ InsideLookingOut/Assets/Scripts/moveCameraScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveCameraScript : MonoBehaviour {
+
+
+    public GameObject cam;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            cam.transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y-10f, cam.transform.position.z);
+        }
+    }
+}
